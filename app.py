55ba4e41e3a05f815319b591725bfb81ed2bf271
Danny <1053784@hr.nl>
2023-12-06 21:26:17 +0100

    Feature: Pagination

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import csv
 
 app = Flask(__name__)
@@ -9,10 +9,21 @@ def index():
     with open('deMaas.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=';')
         data = [row for row in reader]
-        
 
-    # Render the template with the data
-    return render_template('index.html', data=data)
+    # Get the page number from the request query parameter
+    page = int(request.args.get('page', 1))
+    items_per_page = 200
+    start_index = (page - 1) * items_per_page
+    end_index = start_index + items_per_page
+
+    # Get the data for the current page
+    current_data = data[start_index:end_index]
+
+    # Calculate the total number of pages
+    total_pages = (len(data) + items_per_page - 1) // items_per_page
+
+    # Render the template with the data and pagination information
+    return render_template('index.html', data=current_data, page=page, total_pages=total_pages)
 
 if __name__ == '__main__':
     app.run(debug=True)
