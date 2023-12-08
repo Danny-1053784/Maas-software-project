027480460499b443bed93356d10143b806b5c18c
BryanBooij <bryan.booij@xs4all.nl>
2023-12-08 13:06:34 +0100

    start searchbar

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import csv
+from datetime import datetime 
 
 app = Flask(__name__)
 
@@ -24,6 +25,16 @@ def index():
 
     # Render the template with the data and pagination information
     return render_template('index.html', data=current_data, page=page, total_pages=total_pages)
+
+
+
+def search(data): 
+    query_date = request.form.get('query_date')
+    formated_date = datetime.strptime(query_date, '%Y-%m-%d').date() if query_date else None
+
+    results = [data for data in data if not formated_date or datetime.strptime(data['date'], '%Y-%m-%d').date() == formated_date]
+
+    return render_template('index.html', data=results, query_date=query_date)
 
 if __name__ == '__main__':
     app.run(debug=True)
